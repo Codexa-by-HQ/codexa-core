4be757dddaefc1e676c6c63bcccb17b9cb6bf9b8
Hamza-Qureshi <noman97301@gmail.com>
2026-07-18 15:32:31 +0500

    bump: release 0.0.9

--- mod.ts
+++ mod.ts
@@ -18,7 +18,7 @@
  */
 
 /** Current package version published by this source tree. */
-export const CODEXA_CORE_VERSION = '0.0.8';
+export const CODEXA_CORE_VERSION = '0.0.9';
 
 /** Public subpath module names exported by `@codexa/core`. */
 export type CodexaCoreModule =
