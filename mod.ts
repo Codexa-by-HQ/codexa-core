6fe78179bc367ee0c237a0ac95b2cb6a4a0a16bb
Hamza-Qureshi <noman97301@gmail.com>
2026-09-11 00:36:20 +0500

    release v1.0.8

--- mod.ts
+++ mod.ts
@@ -18,7 +18,7 @@
  */
 
 /** Current package version published by this source tree. */
-export const CODEXA_CORE_VERSION = '1.0.7';
+export const CODEXA_CORE_VERSION = '1.0.8';
 
 /** Public subpath module names exported by `@codexa/core`. */
 export type CodexaCoreModule =
